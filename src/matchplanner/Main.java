8a8fbc281e961a6c768c2c162cb77f62063801e0
Marcel Henning <Marcel@Marcels-MacBook-Pro-2.local>
2018-11-27 19:25:45 +0100

    add JtabbedPane design

--- src/matchplanner/Main.java
+++ src/matchplanner/Main.java
@@ -2,12 +2,22 @@ package matchplanner;
 
 import java.awt.EventQueue;
 
+import javax.swing.UIManager;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		
 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				try { 
+			        UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"); 
+			    } catch(Exception ignored){}
+
+			    
+			  
 				try {
 					new MatchplanerGUI();
 
@@ -18,22 +28,22 @@ public class Main {
 		});
 
 		// Hier wird getestet
-		League instance = new League(18);
-		String teams[] = { "Arminia Bielefeld", "1. FSV Mainz 05", "VfB Stuttgart", "1. FC Kaiserslautern",
-				"Borussia Dortmund", "Borussia Mönchengladbach", "Bayer 04 Leverkusen", "Hertha BSC", "1. FC Nürnberg",
-				"Hannover 96", "Eintracht Frankfurt", "FC Bayern München", "VfL Wolfsburg", "FC Schalke 04",
-				"MSV Duisburg", "1. FC Köln", "Werder Bremen", "Hamburger SV" };
-
-		for (int i = 0; i < 2 * instance.getTeams() - 2; i++) {
-			System.out.printf("%d. Spieltag %b%n", i + 1, i < instance.getTeams() - 1);
-			for (int j = 0; j < instance.getTeams() / 2; j++) {
-				Match m = instance.getMatch(j);
-				if (m.getHome() == 0 || m.getGuest() == 0 || m.getHome() == 17 || m.getGuest() == 17) {
-					System.out.printf("%25s <-> %25s%n", teams[m.getHome()], teams[m.getGuest()]);
-				}
-			}
-			instance.shift();
-		}
+//		League instance = new League(18);
+//		String teams[] = { "Arminia Bielefeld", "1. FSV Mainz 05", "VfB Stuttgart", "1. FC Kaiserslautern",
+//				"Borussia Dortmund", "Borussia Mönchengladbach", "Bayer 04 Leverkusen", "Hertha BSC", "1. FC Nürnberg",
+//				"Hannover 96", "Eintracht Frankfurt", "FC Bayern München", "VfL Wolfsburg", "FC Schalke 04",
+//				"MSV Duisburg", "1. FC Köln", "Werder Bremen", "Hamburger SV" };
+//
+//		for (int i = 0; i < 2 * instance.getTeams() - 2; i++) {
+//			System.out.printf("%d. Spieltag %b%n", i + 1, i < instance.getTeams() - 1);
+//			for (int j = 0; j < instance.getTeams() / 2; j++) {
+//				Match m = instance.getMatch(j);
+//				if (m.getHome() == 0 || m.getGuest() == 0 || m.getHome() == 17 || m.getGuest() == 17) {
+//					System.out.printf("%25s <-> %25s%n", teams[m.getHome()], teams[m.getGuest()]);
+//				}
+//			}
+//			instance.shift();
+//		}
 	}
 
 }
--- src/matchplanner/MatchplanerGUI.java
+++ src/matchplanner/MatchplanerGUI.java
@@ -6,6 +6,7 @@
 package matchplanner;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -21,6 +22,8 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -28,138 +31,95 @@ import java.awt.event.MouseEvent;
  */
 public class MatchplanerGUI extends javax.swing.JFrame {
 
-	private JFrame frame;
-
-
 
 	public MatchplanerGUI() {
 		/*
 		 * frame
 		 */
-		frame = new JFrame();
-		frame.setTitle("Matchplanner");
-		frame.setBounds(100, 100, 800, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		frame.setVisible(true);
-
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		panel.setLayout(gbl_panel);
-
-		gbl_panel.columnWidths = new int[] { 12, 516, 232, 0, 0 };
-		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0 };
-		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
-		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
-
-		JLabel lbloutput = new JLabel("");
-		GridBagConstraints gbc_lbloutput = new GridBagConstraints();
-		gbc_lbloutput.gridheight = 5;
-		gbc_lbloutput.insets = new Insets(0, 0, 0, 5);
-		gbc_lbloutput.gridx = 1;
-		gbc_lbloutput.gridy = 1;
-		panel.add(lbloutput, gbc_lbloutput);
-
-		JLabel lblspiel1 = new JLabel("Spieltag 1");
-		lblspiel1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				lbloutput.setText("Spieltag 1");
-			}
-		});
-		GridBagConstraints gbc_lblspiel1 = new GridBagConstraints();
-		gbc_lblspiel1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblspiel1.gridx = 2;
-		gbc_lblspiel1.gridy = 0;
-		panel.add(lblspiel1, gbc_lblspiel1);
-
-		JLabel lblspiel2 = new JLabel("Spieltag 2");
-		lblspiel2.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				lbloutput.setText("Spieltag 2");
-			}
-		});
-		GridBagConstraints gbc_lblspiel2 = new GridBagConstraints();
-		gbc_lblspiel2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblspiel2.gridx = 2;
-		gbc_lblspiel2.gridy = 1;
-		panel.add(lblspiel2, gbc_lblspiel2);
-		
-		
+		super();
+		this.setTitle("Matchplanner");
+		this.setSize(400, 300);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout(0, 0));
+		this.setVisible(true);
 
 		/*
 		 * menubar with actionlisteners
 		 */
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
 
+		JMenuBar menuBar = new JMenuBar();
+		this.add(menuBar, BorderLayout.NORTH);
+
+		//Menuitem File
 		JMenu mnDatei = new JMenu("Datei");
 		menuBar.add(mnDatei);
 
 		JMenuItem mntmNeu = new JMenuItem("Neu");
-		mntmNeu.addActionListener((e)-> {
-				String message = "=> neuen Spielplan anlegen";
-				JOptionPane.showMessageDialog(null, message);
-			}
-		);
+		mntmNeu.addActionListener((e) -> {
+			String message = "=> neuen Spielplan anlegen";
+			JOptionPane.showMessageDialog(null, message);
+		});
 		mnDatei.add(mntmNeu);
 
 		JMenuItem mntmoffnen = new JMenuItem("Öffnen");
-		mntmoffnen.addActionListener((e)-> {
-				String message = "=> vorhandenen Spielplan öffnen";
-				JOptionPane.showMessageDialog(null, message);
-			}
-		);
+		mntmoffnen.addActionListener((e) -> {
+			String message = "=> vorhandenen Spielplan öffnen";
+			JOptionPane.showMessageDialog(null, message);
+		});
 		mnDatei.add(mntmoffnen);
 
 		JMenuItem mntmSpeichern = new JMenuItem("Speichern");
 		mntmSpeichern.addActionListener(e -> {
-				String message = "=> Aenderungen am Spielplan speichern";
-				JOptionPane.showMessageDialog(null, message);
-			}
-		);
-		
+			String message = "=> Aenderungen am Spielplan speichern";
+			JOptionPane.showMessageDialog(null, message);
+		});
+
 		mnDatei.addSeparator();
 		mnDatei.add(mntmSpeichern);
 
 		JMenuItem mntmSpeichernUnter = new JMenuItem("Speichern unter");
-		mntmSpeichernUnter.addActionListener((e)-> {
-				String message = "=> geöffneten Spielplan als neue Datei speichern";
-				JOptionPane.showMessageDialog(null, message);
-			}
-		);
+		mntmSpeichernUnter.addActionListener((e) -> {
+			String message = "=> geöffneten Spielplan als neue Datei speichern";
+			JOptionPane.showMessageDialog(null, message);
+		});
 		mnDatei.add(mntmSpeichernUnter);
-		
+
 		mnDatei.addSeparator();
-		
+
 		JMenuItem mntmBeenden = new JMenuItem("Beenden");
-		mntmBeenden.addActionListener((e) ->{
-				System.exit(0);
-			}
-		);
+		mntmBeenden.addActionListener((e) -> {
+			System.exit(0);
+		});
 		mnDatei.add(mntmBeenden);
+
+//		menuitem Extras
 
 		JMenu mnExtras = new JMenu("Extras");
 		menuBar.add(mnExtras);
-		
 
 		JMenuItem mntmManschaften = new JMenuItem("Manschaften bearbeiten");
-		mntmManschaften.addActionListener((e)-> {
-				String message = "=> Mannschaften verändern";
-				JOptionPane.showMessageDialog(null, message);
-			}
-		);
+		mntmManschaften.addActionListener((e) -> {
+			String message = "=> Mannschaften verändern";
+			JOptionPane.showMessageDialog(null, message);
+		});
 		mnExtras.add(mntmManschaften);
 
 		JMenuItem mntmSpieltage = new JMenuItem("Spieltage bearbeiten");
-		mntmSpieltage.addActionListener((e)-> {
-				String message = "=> Spieltage festlegen/verändern";
-				JOptionPane.showMessageDialog(null, message);
-			}
-		);
+		mntmSpieltage.addActionListener((e) -> {
+			String message = "=> Spieltage festlegen/verändern";
+			JOptionPane.showMessageDialog(null, message);
+		});
 		mnExtras.add(mntmSpieltage);
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.RIGHT);
+		this.add(tabbedPane, BorderLayout.CENTER);
+
+		JLabel test = new JLabel("test");
+		JLabel test2 = new JLabel("test");
+		test.setHorizontalTextPosition(SwingConstants.RIGHT);
+		tabbedPane.addTab("testtitle", test);
+		tabbedPane.addTab("testtitle2", test2);
 
 	}
 
