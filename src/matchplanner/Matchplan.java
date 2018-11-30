b6fd09dcb488d086de182a31cdf03cdb42c1d9bb
JA4N <jan.lindheimer@hotmail.com>
2018-11-30 14:57:01 +0100

    Berechnung Samstage ab dem heutigen Datum

--- src/matchplanner/Matchplan.java
+++ src/matchplanner/Matchplan.java
@@ -1,25 +1,30 @@
 package matchplanner;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-import java.util.HashMap;
 
 
 public class Matchplan {
-	
-	HashMap <Date, Matchday> season = new HashMap();
+
+	HashMap<Date, Matchday> season = new HashMap();
 	List<Team> teams = new ArrayList();
+	List matchday = new ArrayList();
 	League mLeague;
+	//Day Today
+	GregorianCalendar calendar = new GregorianCalendar(TimeZone.getDefault());
+	Date date = calendar.getTime();
+	// return: 1-7 (Saturday = 7)
+	int day = calendar.get(Calendar.DAY_OF_WEEK);
 
 	/*
 	 * Für import aus CSV datei
 	 */
 	public Matchplan(List<Team> teams) {
 		this.mLeague = new League(teams.size());
-		this.teams=teams;
+		this.teams = teams;
 		createPlan();
+
 	}
 
 	/*
@@ -30,7 +35,7 @@ public class Matchplan {
 		return teams.size();
 	}
 
-	
+
 	/*
 	 * Erstellt eine Liga für die angegebene Anzahl von Teams
 	 */
@@ -42,44 +47,47 @@ public class Matchplan {
 	 * Erstellt dein eigentlichen Matchplan
 	 */
 	public void createPlan() {
-		Date defaultDate = new Date(0000, 00 ,00);
+		Date defaultDate = new Date(0000, 00, 00);
 
 		for (int i = 0; i < mLeague.getTeams() - 1; i++) {
-			List <Match> tempM = new ArrayList();
+			List<Match> tempM = new ArrayList();
 			for (int j = 0; j < mLeague.getTeams() / 2; j++) {
 				Match m = mLeague.getMatch(j);
 				tempM.add(m);
 			}
 			season.put(defaultDate, new Matchday(tempM));
-			defaultDate.setDate(defaultDate.getDate()+1);
+			defaultDate.setDate(defaultDate.getDate() + 1);
 			mLeague.shift();
 		}
 
 		mLeague = new League(mLeague.getTeams());
 
-		for (int l = mLeague.getTeams(); l < 2 * mLeague.getTeams() -1; l++) {
-			List <Match> tempM = new ArrayList();
+		for (int l = mLeague.getTeams(); l < 2 * mLeague.getTeams() - 1; l++) {
+			List<Match> tempM = new ArrayList();
 			for (int k = 0; k < mLeague.getTeams() / 2; k++) {
 				Match m = mLeague.getMatch(k);
 				tempM.add(m);
 			}
 			season.put(defaultDate, new Matchday(tempM));
-			defaultDate.setDate(defaultDate.getDate()+1);
+			defaultDate.setDate(defaultDate.getDate() + 1);
 			mLeague.shift();
 
 		}
 
 	}
-	
-	
+
+
 	//Jan
 	//Bitte Liste erzeugen, die die Datums nächsten Samstage ab dem aktuellen datum speichert
 	//anzahl der Samstag ist anzahl spieltage
-	public ArrayList<Date> createDates(int spieltage){
-		throw new NotImplementedException();
+	public ArrayList<Date> createDates(int spieltage) {
+		for (int i = 0; i <= spieltage; i++) {
+			if (day == Calendar.SATURDAY) {
+				//System.out.println(day);
+				matchday.add(day);
+			}
+		}
+		return (ArrayList<Date>) matchday;
+
 	}
-	
-	
-
-
-}
+	}
