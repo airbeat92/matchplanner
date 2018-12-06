f7b7fce1739a43c7308e297dcb7878f7725b497c
81klma1bif <81klma1bif@hft-stuttgart.de>
2018-12-06 01:14:51 +0100

    update Writer

---
+++ src/matchplanner/CSVWriter.java
@@ -0,0 +1,62 @@
+package matchplanner;
+
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVWriter {
+
+public static void writeCsv(String filePath) {
+List <Team> teamList = new ArrayList(mp.getTeams());
+List <Team> dateList = new ArrayList();
+for ( LocalDate ld : mp.getSeason()) {
+    dateList.add(ld.getMatchDay());
+  }
+  FileWriter fileWriter = null;
+  try{
+  fileWriter = new FileWriter(filepath);
+  
+  fileWriter.append("#matchplan " + mp.getMatchplanName());
+  fileWirter.append(\n);
+  fileWriter.append("#teamsize " + mp.teams.size());
+  fileWirter.append(\n);
+  fileWriter.append("#teams");
+  fileWirter.append(\n);
+  fileWriter.append("Id, Name, Shortname");
+  
+  for ( Team t : teamList) {
+  fileWriter.append(String.valueOf(t.getId());
+  fileWriter.append(";");
+  fileWriter.append(String.valueOf(t.getName());
+  fileWriter.append(";");
+  fileWriter.append(String.valueOf(t.getShortname());
+  fileWriter.append("\n");
+  
+  }
+  
+  fileWirter.append("#matchdate");
+  for ( LocalDate ld : dateList)
+ // fileWirter.append(); // Datum entsprechend ausgeben
+} catch (Exception e) {
+e.printStackTrace();
+} finally {
+    try {
+        fileWriter.flush();
+        fileWriter.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+ 
+}
+
+
+
+
+
+
+
+
+
+}
